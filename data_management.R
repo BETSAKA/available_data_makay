a26bd81d3a93ae73cb26e0b429bd497f1ec235bc
fBedecarrats <f.bedecarrats@gmail.com>
2024-04-07 15:41:36 +0200

    Refont d'1 notebook vers un site.

--- data_management.R
+++ data_management.R
@@ -7,3 +7,22 @@ firms_files <- list.files(path = "data/nasa_firms", recursive = TRUE,
 firms_dest <- str_replace(firms_files, "data/", "mapme_biodiversity/")
 
 map2(firms_files, firms_dest, put_to_s3)
+
+
+# Check which SRTM overlap with MAdagascar --------------------------------
+
+
+overlaps_with_mada <- function(raster_file, contour_mada) {
+  # Load the raster file
+  r <- raster(raster_file)
+  
+  # Convert the raster to an sf object
+  r_sf <- st_as_sf(r, na.rm = TRUE)
+  
+  # Check if there's any intersection between the raster and contour_mada
+  any(st_intersects(r_sf, contour_mada))
+}
+
+files <- list.files("data/mapme_biodiversity/nasa_srtm", full.names = TRUE)
+
+overlapping_files <- files[map_lgl(files, overlaps_with_mada, contour_mada = contour_mada)]
